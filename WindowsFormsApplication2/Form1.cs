58cf94e38774200deea87112cb9a0e92402060e0
Jobonno <jobonno@gmail.com>
2017-03-02 21:58:12 +1100

    Add project files.

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string era = "BC";
+            
+            string result = "";
+            for (int i = -5; i <= 5; i++)
+            {
+                if(era == "BC" && i < -1)
+                {
+                    i *= -1;
+                    result += "Century " + i + " " + era + " " + (i - 1) * 100 + "-" + (i * 100 - 1) +"\n";
+                    i *= -1;
+                }else if(era == "BC" && i == 1)
+                {
+                    result += "Century " + i + " " + era + " " + i + "-" + (i * 100 - 1) + "\n";
+                    era = "AD";
+                    i -= 1;
+                }else if(era == "AD" && i == 1)
+                {
+                    result += "Century " + i + " " + era + " " + i + "-" + (i * 100 - 1) + "\n";
+                    
+                }else if(era == "AD" && i < 6)
+                {
+                    result += "Century " + i + " " + era + " " + (i - 1) * 100 + "-" + (i * 100 - 1) + "\n";
+                }
+                
+            }
+            MessageBox.Show(result);
+        }
+    }
+}
